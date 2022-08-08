4c07a5e4752dccded09d9f8f2ccb7f314a3a03ca
Liaksej <tarusov.liaksej@gmail.com>
2022-08-08 23:26:53 +0200

    Improve structure of files

--- musicservice_create_req.sql.sql
+++ musicservice_create_req.sql.sql
@@ -1,45 +1,50 @@
-CREATE TABLE IF NOT EXISTS Genres (
-	genre_id SERIAL PRIMARY KEY,
-	name VARCHAR(80) UNIQUE NOT NULL
+CREATE TABLE IF NOT EXISTS genres (
+		PRIMARY KEY (genre_id),
+		genre_id SERIAL,
+		name     VARCHAR(80) UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Artists (
-	artist_id SERIAL PRIMARY KEY,
-	name_or_alias VARCHAR(160)
+CREATE TABLE IF NOT EXISTS artists (
+    PRIMARY KEY (artist_id),
+		artist_id     SERIAL,
+    name_or_alias VARCHAR(160)
 );
 
-CREATE TABLE IF NOT EXISTS GenresArtists (
-	artist_id INTEGER REFERENCES Artists,
-	genre_id INTEGER REFERENCES Genres,
-  PRIMARY KEY(artist_id, genre_id)
+CREATE TABLE IF NOT EXISTS genres_artists (
+    PRIMARY KEY (artist_id, genre_id),
+		artist_id INTEGER REFERENCES artists,
+    genre_id  INTEGER REFERENCES genres
 );
 
-CREATE TABLE IF NOT EXISTS Albums (
-	album_id SERIAL PRIMARY KEY,
-	name VARCHAR(160) UNIQUE NOT NULL,
-  release_year INTEGER NOT NULL
+CREATE TABLE IF NOT EXISTS albums (
+		PRIMARY KEY (album_id),
+		album_id     SERIAL,
+		name         VARCHAR(160) UNIQUE NOT NULL,
+    release_year INTEGER      NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS AlbumsArtists (
-	artist_id INTEGER REFERENCES Artists,
-	album_id INTEGER REFERENCES Albums,
-  PRIMARY KEY(artist_id, album_id)
+CREATE TABLE IF NOT EXISTS albums_artists (
+		PRIMARY KEY(artist_id, album_id),
+		artist_id INTEGER REFERENCES artists,
+	  album_id  INTEGER REFERENCES albums
 );
 
-CREATE TABLE IF NOT EXISTS Tracks (
-	track_id SERIAL PRIMARY KEY,
-	name VARCHAR(160) NOT NULL,
-  album_id INTEGER REFERENCES Albums
+CREATE TABLE IF NOT EXISTS tracks (
+		PRIMARY KEY (track_id),
+		track_id SERIAL,
+	  name     VARCHAR(160) NOT NULL,
+    album_id INTEGER      REFERENCES albums
 );
 
-CREATE TABLE IF NOT EXISTS Compilations (
-	compilation_id SERIAL PRIMARY KEY,
-	name VARCHAR(160) UNIQUE NOT NULL,
-  release_year INTEGER NOT NULL
+CREATE TABLE IF NOT EXISTS compilations (
+		PRIMARY KEY (compilation_id),
+		compilation_id SERIAL,
+	  name           VARCHAR(160) UNIQUE NOT NULL,
+    release_year   INTEGER      NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS TracksCompilations (
-	track_id INTEGER REFERENCES Tracks,
-	compilation_id INTEGER REFERENCES Compilations,
-  PRIMARY KEY(track_id, compilation_id)
+CREATE TABLE IF NOT EXISTS tracks_compilations (
+		PRIMARY KEY(track_id, compilation_id),
+		track_id     INTEGER REFERENCES tracks,
+	compilation_id INTEGER REFERENCES compilations
 );
--- musicservice_select_req.sql
+++ musicservice_select_req.sql
@@ -1,25 +1,34 @@
 -- Запрос названия и года выхода альбомов, вышедших в 2018 году:
-SELECT name, release_year FROM "Albums"
-	WHERE release_year = 2018;
-		
-		
+SELECT name, release_year
+  FROM albums
+ WHERE release_year = 2018;
+
+
 -- Запрос названия и продолжительности самого длительного трека:
-SELECT name, length  FROM "Tracks" t 
-	WHERE length  = (SELECT MAX(length) FROM "Tracks" t2);
-	
+SELECT name, length
+  FROM tracks
+ WHERE length  = (SELECT MAX(length)
+                    FROM tracks);
+
 -- Запрос названия треков, продолжительность которых не менее 3,5 минуты:
-SELECT name FROM "Tracks" t 
-	WHERE length >= '00:03:30';
-	
+SELECT name
+  FROM tracks
+ WHERE length >= '00:03:30';
+
 -- Запрос названия сборников, вышедших в период с 2018 по 2020 год включительно:
-SELECT name FROM "Compilations" c  
-	WHERE release_year >= 2018 AND release_year <= 2020;
-	
+SELECT name
+  FROM compilations
+ WHERE release_year >= 2018
+   AND release_year <= 2020;
+
 -- Запрос исполнителей, чье имя состоит из 1 слова:
-SELECT name_or_alias  FROM "Artists" a   
-	WHERE name_or_alias NOT LIKE '% %';
+SELECT name_or_alias
+  FROM artists
+ WHERE name_or_alias NOT LIKE '% %';
 
 -- Запрос названия треков, которые содержат слово "мой"/"my":
-SELECT "name" FROM "Tracks" t   
-	WHERE "name" LIKE '% мой %' OR "name" LIKE '% my %' OR "name" LIKE '%My %';
-	
+SELECT name
+  FROM tracks
+ WHERE name LIKE '% мой %'
+    OR name LIKE '% my %'
+    OR name LIKE '%My %';
